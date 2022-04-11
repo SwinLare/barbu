cc49cfd1b3980da36382f92f8ef1ec72bea214e8
SwinLare <79059508+SwinLare@users.noreply.github.com>
2022-04-12 00:42:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>La barbe à jojo!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Comment se raser les couilles et en faire une barbes " />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
